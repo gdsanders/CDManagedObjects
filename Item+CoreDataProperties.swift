85ae44f3d631c76925163a953fe15ed1540533d5
gdsanders <gd@digitalequity.com>
2015-10-07 22:30:29 -0500

    Init

---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+//
+//  Item+CoreDataProperties.swift
+//  CDManagedObjects
+//
+//  Created by G.D. Sanders on 10/7/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Item {
+
+
+}
---
+++ Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  CDManagedObjects
+//
+//  Created by G.D. Sanders on 10/7/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Item: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
